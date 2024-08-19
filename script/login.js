823239409cca8f462b6bfa30d5d64e59246549c2
talhahzahid <talhazahid415@gmail.com>
2024-08-19 11:29:01 +0500

    index

--- script/login.js
+++ script/login.js
@@ -1,6 +1,6 @@
 import { 
     signInWithEmailAndPassword,
-    // onAuthStateChanged,
+    onAuthStateChanged,
  } from "https://www.gstatic.com/firebasejs/10.12.5/firebase-auth.js";
 import{auth} from "./config.js"
 
